be6194255c4cf79dfc86adabaafadfdd37609a7e
740291272 <740291272@qq.com>
2018-11-13 22:20:33 +0800

    fix usage bug

--- bin/sinsy.cpp
+++ bin/sinsy.cpp
@@ -37,7 +37,7 @@ void usage()
     std::cout << "    SinsyNG [ options ] [ infile ]" << std::endl;
     std::cout << "  options:                                           [def]" << std::endl;
     std::cout << "    -w langs    : languages                          [ en]" << std::endl;
-    std::cout << "    -s Score File (*.sinsy)                          [N/A]" << std::endl;
+    std::cout << "    -s Score    : Score File (*.sinsy)               [N/A]" << std::endl;
     std::cout << "    -o file     : filename of output wav audio       [N/A]" << std::endl;
     std::cout << "  infile:" << std::endl;
     std::cout << "    MusicXML file" << std::endl;
